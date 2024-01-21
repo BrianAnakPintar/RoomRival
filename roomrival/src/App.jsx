88bcbafc04784db1a1e62d46aac96866cb946157
Jessie Sheng <yjsheng@students.cs.ubc.ca>
2024-01-20 16:26:11 -0800

    added login card

--- roomrival/src/App.jsx
+++ roomrival/src/App.jsx
@@ -3,30 +3,34 @@ import './App.css';
 import io from "socket.io-client";
 import {useEffect} from "react";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  const sendMessage = () => {
-    let username = document.getElementById("username");
-    socket.emit("set_username", username.value);
-  };
+    const sendMessage = () => {
+        let username = document.getElementById("username");
+        socket.emit("set_username", username.value);
+    };
 
-  useEffect(() => {
-    socket.on("receive_user", (data) => {
-      alert(data.id);
-    });
-  }, [socket]);
+    useEffect(() => {
+        socket.on("receive_user", (data) => {
+            alert(data.id);
+        });
+    }, [socket]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <input id="username" />
-        <button onClick={sendMessage}>Send</button>
-        <Login/>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<header className="App-header">*/}
+            {/*</header>*/}
+            <body className="App-body">
+                <input id="username"/>
+                <button onClick={sendMessage}>Send</button>
+                <Login/>
+            </body>
+            <Navbar className="navbar"/>
+        </div>
+    );
 }
 
 export default App;
---
+++ roomrival/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">RoomRival</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Leaderboard</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
